7854bc8d4587d09a3d6730d367cfc5bddf546d28
Dalibex <dalibexyt@gmail.com>
2025-07-06 22:42:35 +0200

    Remove unused mixins and refactor TrinketSlots logic
    Deleted HandledScreenAccessor and TrinketsSlotsSurvivalView mixins, consolidating slot highlight logic into TrinketSlots. Updated mixin configuration files to remove references to the deleted classes and simplified the injection point in TrinketSlots. So now colors in slots works in all UI and is SO much simple than before and should now work when loading game.

--- src/client/java/com/dalibex/trinket/mixin/client/TrinketSlots.java
+++ src/client/java/com/dalibex/trinket/mixin/client/TrinketSlots.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.player.PlayerInventory;
 
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,24 +16,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(HandledScreen.class)
 public abstract class TrinketSlots<T extends ScreenHandler> {
 
-    @Shadow protected int x;
-    @Shadow protected int y;
-    @Shadow protected T handler;
-
-    @Inject(method = "render", at = @At("TAIL"))
-    private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    @Inject(method = "drawSlot", at = @At("HEAD"))
+    private void onDrawSlot(DrawContext context, Slot slot, CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.player == null) return;
 
         PlayerInventory playerInv = client.player.getInventory();
 
-        for (Slot slot : this.handler.slots) {
-            if (slot.inventory == playerInv && slot.getIndex() == 9 || slot.inventory == playerInv && slot.getIndex() == 10) {
-                int slotX = this.x + slot.x;
-                int slotY = this.y + slot.y;
+        if (slot.inventory == playerInv && ( slot.getIndex() == 9 || slot.getIndex() == 10)) {
+            int slotX = slot.x;
+            int slotY = slot.y;
 
-                context.fill(slotX, slotY, slotX + 16, slotY + 16, 0x10FF0000);
-            }
+            context.fill(slotX, slotY, slotX + 16, slotY + 16, 0x10FF0000);
         }
     }
 }
